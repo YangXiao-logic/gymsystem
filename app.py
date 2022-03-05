f32d196e8443af467f8a966241fdaab670df284a
YangXiao-logic <sc19yx@leeds.ac.uk>
2022-03-05 14:53:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-from flask import Flask
+from gymsystem import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app = create_app('production')
