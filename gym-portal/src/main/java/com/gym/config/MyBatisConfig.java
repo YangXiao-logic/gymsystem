1bfcb2f826e16abeb7674997e87a4562f969358c
YangXiao-logic <sc19yx@leeds.ac.uk>
2022-04-16 09:22:30 +0800

    new config

---
+++ gym-portal/src/main/java/com/gym/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package com.gym.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ * Created by macro on 2019/4/8.
+ */
+@Configuration
+@MapperScan({"com.gym.mbg.mapper","com.gym.dao"})
+public class MyBatisConfig {
+}
